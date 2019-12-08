59ff56bed1a6b6f8650d84c147f520f9e02effc9
Vinicius M. Almeida <vinicius.sec21@gmail.com>
2019-12-08 13:27:33 -0300

    padronizando componente

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import PrimeiroComponente from './componentes/PrimeiroComponente'
 
 const elemento = document.getElementById('root')
-const jsx = <h1>Olá React!</h1>
-ReactDOM.render(jsx, elemento)
+ReactDOM.render(
+    <div>
+        <PrimeiroComponente />
+    </div>
+, elemento)
+
+// const jsx = <h1>Olá React!</h1>
+// ReactDOM.render(jsx, elemento)
