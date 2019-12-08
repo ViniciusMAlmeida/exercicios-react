d3aa9d29d7baa9e57372b142052f49262b22f698
Vinicius M. Almeida <vinicius.sec21@gmail.com>
2019-12-08 13:51:43 -0300

    Um arquivo com multiplos componentes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PrimeiroComponente from './componentes/PrimeiroComponente'
+// import PrimeiroComponente from './componentes/PrimeiroComponente'
+import {CompA, CompB as B} from './componentes/DoisComponentes'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <PrimeiroComponente valor="Bom dia!" />
+        <CompA valor="Olá eu sou o A!" />
+        <B valor="B na área!" />
+        {/* <PrimeiroComponente valor="Bom dia!" /> */}
     </div>
 , elemento)
 
