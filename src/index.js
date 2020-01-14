cf84f25dbf78f8f782d8a04e1b38398cc6f9deab
ViniciusMAlmeida <vinicius.almeida@coopertec.com.br>
2020-01-14 13:35:15 -0300

    Sistema de Módulos (Import/Export)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
-// import {CompA, CompB as B} from './componentes/DoisComponentes'
-import MultiElementos from './componentes/MultiElementos'
+import CompA, { CompB as B } from './componentes/DoisComponentes'
+// import MultiElementos from './componentes/MultiElementos'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <MultiElementos />
-        {/* <CompA valor="Olá eu sou o A!" />
-        <B valor="B na área!" /> 
-        <PrimeiroComponente valor="Bom dia!" /> */}
+        {/*<MultiElementos />*/}
+        <CompA valor="Olá eu sou o A!" />
+        <B valor="B na área!" />
+        {/*<PrimeiroComponente valor="Bom dia!" /> */}
     </div>
-, elemento)
+    , elemento)
 
 // const jsx = <h1>Olá React!</h1>
 // ReactDOM.render(jsx, elemento)
