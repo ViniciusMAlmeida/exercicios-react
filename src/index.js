fdea3536af4774fdb26ea0c0f6f5e191ea86e897
ViniciusMAlmeida <vinicius.almeida@coopertec.com.br>
2020-01-16 16:03:24 -0300

    Relação entre Componentes #2

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom'
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
 // import CompA, { CompB as B } from './componentes/DoisComponentes'
 // import MultiElementos from './componentes/MultiElementos'
-import FamiliaSilva from './componentes/FamiliaSilva'
+// import FamiliaSilva from './componentes/FamiliaSilva'
+import Familia from './componentes/Familia'
+import Membro from './componentes/Membro'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <FamiliaSilva />
+        <Familia sobrenome="Pereira">
+            <Membro nome="Andre" />
+            <Membro nome="Mariana" />
+        </Familia>
+        {/*<FamiliaSilva />*/}
         {/*<MultiElementos />*/}
         {/*<CompA valor="Olá eu sou o A!" />
         <B valor="B na área!" />*/}
